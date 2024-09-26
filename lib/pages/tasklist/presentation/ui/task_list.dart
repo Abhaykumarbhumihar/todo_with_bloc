c6b1c4a60fa80b186482e623b9187f80600cacf7
Abhay kumar bhumihar <abhayrai637@gmail.com>
2024-09-26 14:15:24 +0530

    task list show

--- lib/pages/tasklist/presentation/ui/task_list.dart
+++ lib/pages/tasklist/presentation/ui/task_list.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:todoapp/pages/tasklist/presentation/bloc/task_list_bloc.dart';
+import 'package:todoapp/pages/tasklist/presentation/ui/task_list_data.dart';
 
 import '../../../../common/component/home_component/home_componnt.dart';
 import '../../../../common/responsive/screenUtils.dart';
@@ -28,90 +29,7 @@ class TaskList extends StatelessWidget {
             child: Column(
               children: [
                 appBarcommono(context: context, title: "Task Lists"),
-                Flexible(child: BlocBuilder<TaskListBloc, TaskListState>(
-                  builder: (context, state) {
-                    return ListView.builder(
-                        itemCount: state.addTodoTask?.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            children: <Widget>[
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(
-                                  left: screenWidth * 0.04,
-                                  right: screenWidth * 0.04,
-                                  bottom: screenWidth * 0.06,
-                                ),
-                                child: Container(
-                                  width: screenWidth,
-                                  padding: const EdgeInsets.all(16.0),
-                                  decoration: BoxDecoration(
-                                    color: AppColor.backgroundContainerColor,
-                                    borderRadius: BorderRadius.circular(16.0),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black.withOpacity(0.5),
-                                        spreadRadius: 1,
-                                        blurRadius: 5,
-                                        offset: const Offset(0, 1),
-                                      ),
-                                    ],
-                                  ),
-                                  child: Stack(
-                                    children: <Widget>[
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Text(
-                                            "${state.addTodoTask![index].title}",
-                                            style: TextStyle(
-                                              color: AppColor.appTitleTextColor,
-                                              fontFamily: 'Poppins SemiBold',
-                                              fontSize: screenWidth * 0.04,
-                                            ),
-                                          ),
-                                          Text(
-                                            "Wed, Oct 16, 2024",
-                                            style: TextStyle(
-                                                color: AppColor.appSubTextColor,
-                                                fontFamily: 'Poppins Regular',
-                                                fontSize: screenWidth * 0.03),
-                                          ),
-                                        ],
-                                      ),
-                                      Positioned(
-                                        right: 0.0,
-                                        top: screenHeight * 0.019,
-                                        child: Row(
-                                          children: <Widget>[
-                                            Icon(
-                                              FontAwesomeIcons.pencil,
-                                              color: Colors.white,
-                                              size: screenWidth * 0.06,
-                                            ),
-                                            const SizedBox(
-                                              width: 20,
-                                            ),
-                                            Icon(
-                                              FontAwesomeIcons.trash,
-                                              color: Colors.white,
-                                              size: screenWidth * 0.06,
-                                            )
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          );
-                        });
-                  },
-                ))
+                TaskListView()
               ],
             ),
           ),
---
+++ lib/pages/tasklist/presentation/ui/task_list_data.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../common/responsive/screenUtils.dart';
+import '../../../../common/values/app_color.dart';
+import '../bloc/task_list_bloc.dart';
+
+class TaskListView extends StatelessWidget {
+  const TaskListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = ScreenUtils.width(context);
+    double screenHeight = ScreenUtils.height(context);
+    return Flexible(child: BlocBuilder<TaskListBloc, TaskListState>(
+      builder: (context, state) {
+        return ListView.builder(
+            itemCount: state.addTodoTask?.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: screenWidth * 0.04,
+                      right: screenWidth * 0.04,
+                      bottom: screenWidth * 0.06,
+                    ),
+                    child: Container(
+                      width: screenWidth,
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: AppColor.backgroundContainerColor,
+                        borderRadius: BorderRadius.circular(16.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: const Offset(0, 1),
+                          ),
+                        ],
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "${state.addTodoTask![index].title}",
+                                style: TextStyle(
+                                  color: AppColor.appTitleTextColor,
+                                  fontFamily: 'Poppins SemiBold',
+                                  fontSize: screenWidth * 0.04,
+                                ),
+                              ),
+                              Text(
+                                "Wed, Oct 16, 2024",
+                                style: TextStyle(
+                                    color: AppColor.appSubTextColor,
+                                    fontFamily: 'Poppins Regular',
+                                    fontSize: screenWidth * 0.03),
+                              ),
+                            ],
+                          ),
+                          Positioned(
+                            right: 0.0,
+                            top: screenHeight * 0.019,
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  FontAwesomeIcons.pencil,
+                                  color: Colors.white,
+                                  size: screenWidth * 0.06,
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Icon(
+                                  FontAwesomeIcons.trash,
+                                  color: Colors.white,
+                                  size: screenWidth * 0.06,
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              );
+            });
+      },
+    ));
+  }
+}
